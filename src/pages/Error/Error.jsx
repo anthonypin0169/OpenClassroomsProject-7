27297951fde706f7690c2c4d946be6747ae8babd
Anthony Pin <anthonypin0169@gmail.com>
2026-01-21 00:21:52 +0100

    ajout de la page erreur

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,8 +1,15 @@
 import React from "react"
 import "./Error.css"
+import { NavLink } from "react-router-dom"
 
 function Error() {
-  return <h1>404</h1>
+  return (
+    <main className="error">
+      <h1 className="error__h1">404</h1>
+      <h2 className="error__h2">Oups! La page que vous demandez n'existe pas.</h2> 
+      <NavLink to="/" className="error__link">Retourner sur la page d'acceuil</NavLink> 
+    </main>
+  )
 
 }
 
